5c807592fd0cb2f9bfbf04454d5093076dd72bc4
seongguk <live8681@naver.com>
2021-09-10 00:46:34 +0900

    #02 datatype add

--- CsharpBasicExample/Program.cs
+++ CsharpBasicExample/Program.cs
@@ -21,15 +21,20 @@ namespace DataTypeExample
 
             #region 정수
             IntExample1();
+            IntExample2();
+            #endregion
+
+            #region 논리값 자료형 
+            BoolExample1();
             #endregion
         }
 
-        #region string
+        #region 문자
 
         /// <summary>
         /// String
         /// </summary>
-        public static void StringExample1()
+        private static void StringExample1()
         {
             string name = "seongguk";
             string ment = $"my name is {name}";//문자열 보간
@@ -39,7 +44,7 @@ namespace DataTypeExample
         /// <summary>
         /// StringBuilder
         /// </summary>
-        public static void StringExample2()
+        private static void StringExample2()
         {
             StringBuilder sb = new StringBuilder();
             sb.Append("A");
@@ -55,12 +60,12 @@ namespace DataTypeExample
 
         #endregion
 
-        #region int
+        #region 정수
 
         /// <summary>
         /// int
         /// </summary>
-        public static void IntExample1()
+        private static void IntExample1()
         {
             string strNum = "123";
             //정규식 사용하여 숫자인지 판단 후 int형으로 변경
@@ -75,7 +80,48 @@ namespace DataTypeExample
             }
         }
 
+
+        /// <summary>
+        /// double, decimal
+        /// </summary>
+        private static void IntExample2()
+        {
+            Double[] values = { 10.0, 2.88, 2.88, 2.88, 9.0 };
+            Double result = 27.64;
+            Double total = 0;
+            foreach (var value in values)
+                total += value;
+
+            if (total.Equals(result))
+                Console.WriteLine("같음");
+            else
+                Console.WriteLine($"더한 값 : ({total}) 결과 : ({result}).");
+
+            decimal deResult = (decimal)result;
+            decimal detotal = (decimal)total;
+
+            if (deResult.Equals(detotal))
+                Console.WriteLine("같음");
+            else
+                Console.WriteLine($"더한 값 : ({deResult}) 결과 : ({detotal}).");
+        }
+
         #endregion
 
+        #region 논리값 자료형
+        private static void BoolExample1()
+        {
+            bool isRed = true;
+
+            if (isRed)
+            {
+                Console.WriteLine($"빨간색입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"빨간색이 아닙니다.");
+            }
+        }
+        #endregion
     }
 }
