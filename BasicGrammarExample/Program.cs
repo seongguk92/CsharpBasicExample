7e3d74e6c67d79e75b45bcd52a64c42ec6714db2
seongguk <live8681@naver.com>
2021-09-12 23:22:05 +0900

    #2 필수문법 프로젝트 추가

---
+++ BasicGrammarExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicGrammarExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
