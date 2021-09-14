7c15200ffdc77af925ac0b4bd42887c8ec4fa02a
seongguk <live8681@naver.com>
2021-09-14 21:48:16 +0900

    [setting] Make DataStructure project

---
+++ DataStructureExample/Program.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+
+namespace DataStructureExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dictionary <key, value>
+            #endregion
+            #region List < T >
+            #endregion
+            #region Queue < T >
+            #endregion
+            #region Stack < T >
+            #endregion
+            #region LinkedList < T >
+            #endregion
+            #region SortedList<TKey, TValue>
+            #endregion
+        }
+    }
+}
